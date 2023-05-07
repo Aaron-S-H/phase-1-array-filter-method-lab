54cdd76ed369bc79886c7cec61b479de371159ce
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-05-07 19:33:19 +0200

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+
+
+
+function findMatching(array, name) {
+    const matchDrivers = array.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    return matchDrivers;
+}
+
+function fuzzyMatch(array, name) {
+    const fuzzyDrivers = array.filter(driver => driver[0] === name[0]);
+    return fuzzyDrivers;
+}
+
+
+function matchName(object, name) {
+    const sameThings = object.filter(things => things.name === name);
+        return sameThings;
+}
+
